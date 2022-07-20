d2a61336082ed43c90e43f4391f61937d9445bed
Marcos F. Alves <marcosf_alves@outlook.com>
2022-07-20 08:40:09 -0300

    Baby Steps - wrong use

--- src/main/java/service/SalaryCalculator.java
+++ src/main/java/service/SalaryCalculator.java
@@ -1,10 +1,15 @@
 package service;
 
 import model.Functionary;
+import model.Role;
 
 public class SalaryCalculator {
 
     public double calculateSalary(Functionary functionary){
-        return 1350.00;
+        if (functionary.getRole() == Role.DEVELOPER) {
+            if (functionary.getSalary() > 3000) return 3200.00;
+            return 1350.00;
+        }
+        return 425.00;
     }
 }
--- src/test/java/service/SalaryCalculatorTest.java
+++ src/test/java/service/SalaryCalculatorTest.java
@@ -18,4 +18,24 @@ public class SalaryCalculatorTest {
         assertEquals(1500.00 * 0.9, salary, 0.00001);
     }
 
+    @Test
+    public void ShouldCalculateSalaryForDevelopersWithSalaryAboveLimit(){
+        SalaryCalculator calculator = new SalaryCalculator();
+        Functionary developer = new Functionary("Marcos", 4000.00, Role.DEVELOPER);
+
+        double salary = calculator.calculateSalary(developer);
+
+        assertEquals(4000.00 * 0.8, salary, 0.00001);
+    }
+
+    @Test
+    public void ShouldCalculateSalaryForDBAWithSalaryBelowLimit(){
+        SalaryCalculator calculator = new SalaryCalculator();
+        Functionary developer = new Functionary("Marcos", 500.00, Role.DBA);
+
+        double salary = calculator.calculateSalary(developer);
+
+        assertEquals(500.00 * 0.85, salary, 0.00001);
+    }
+
 }
