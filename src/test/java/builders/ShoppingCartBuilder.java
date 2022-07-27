66d12315adf42375df1968279e3ccd8d6abd48f5
Marcos F. Alves <marcosf_alves@outlook.com>
2022-07-27 08:20:36 -0300

    Test Data Builders

---
+++ src/test/java/builders/ShoppingCartBuilder.java
@@ -0,0 +1,24 @@
+package builders;
+
+import model.Item;
+import model.ShoppingCart;
+
+public class ShoppingCartBuilder {
+
+    public ShoppingCart cart;
+
+    public ShoppingCartBuilder() {
+        this.cart = new ShoppingCart();
+    }
+
+    public ShoppingCartBuilder withItems(double... values) {
+        for (double value : values){
+            cart.add(new Item("Item", 1, value));
+        }
+        return this;
+    }
+
+    public ShoppingCart build() {
+        return cart;
+    }
+}
--- src/test/java/service/ShoppingCartTest.java
+++ src/test/java/service/ShoppingCartTest.java
@@ -2,6 +2,7 @@ package service;
 
 import static org.junit.Assert.*;
 
+import builders.ShoppingCartBuilder;
 import model.Item;
 import model.ShoppingCart;
 import org.junit.Before;
@@ -30,9 +31,13 @@ public class ShoppingCartTest {
 
     @Test
     public void shouldReturnMajorAmountWhenShoppingCartHasManyElements(){
-        cart.add(new Item("Refrigerator", 1, 900.00));
+        cart = new ShoppingCartBuilder()
+                .withItems(900.00, 1500.00, 750.00)
+                .build();
+
+        /*cart.add(new Item("Refrigerator", 1, 900.00));
         cart.add(new Item("Stove", 1, 1500.00));
-        cart.add(new Item("Washer", 1, 750.00));
+        cart.add(new Item("Washer", 1, 750.00));*/
 
         assertEquals(1500.00, cart.highestTotalValue(), 0.0001);
     }
