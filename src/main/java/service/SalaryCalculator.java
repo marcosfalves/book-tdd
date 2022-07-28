d3daf1265bbb68396bf7aa6f8911baccae00cbb4
Marcos F. Alves <marcosf_alves@outlook.com>
2022-07-28 08:21:53 -0300

    TDD and Cohesion - Refactor Salary Calculator

--- src/main/java/service/SalaryCalculator.java
+++ src/main/java/service/SalaryCalculator.java
@@ -7,18 +7,26 @@ public class SalaryCalculator {
 
     public double calculateSalary(Functionary functionary){
         if (functionary.getRole().equals(Role.DEVELOPER)){
-            if (functionary.getSalary() > 3000){
-                return functionary.getSalary() * 0.8;
-            }
-            return functionary.getSalary() * 0.9;
+            return tenOrTwentyPercentDiscount(functionary);
         } else if (functionary.getRole().equals(Role.DBA)
                     || functionary.getRole().equals(Role.TESTER)) {
-            if (functionary.getSalary() < 2500){
-                return functionary.getSalary() * 0.85;
-            }
-            return functionary.getSalary() * 0.75;
+            return fifteenOrTwentyFivePercentDiscount(functionary);
         }
 
-        throw new RuntimeException("Funcionário com cargo inválido");
+        throw new RuntimeException("Functionary with invalid role");
+    }
+
+    private double fifteenOrTwentyFivePercentDiscount(Functionary functionary) {
+        if (functionary.getSalary() < 2500){
+            return functionary.getSalary() * 0.85;
+        }
+        return functionary.getSalary() * 0.75;
+    }
+
+    private double tenOrTwentyPercentDiscount(Functionary functionary){
+        if (functionary.getSalary() > 3000){
+            return functionary.getSalary() * 0.8;
+        }
+        return functionary.getSalary() * 0.9;
     }
 }
