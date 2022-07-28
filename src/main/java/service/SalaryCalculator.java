5311344bf396d7c6654b818a3b5d0828178f472d
Marcos F. Alves <marcosf_alves@outlook.com>
2022-07-28 08:15:49 -0300

    TDD and Cohesion - Salary Calculator

--- src/main/java/service/SalaryCalculator.java
+++ src/main/java/service/SalaryCalculator.java
@@ -6,9 +6,19 @@ import model.Role;
 public class SalaryCalculator {
 
     public double calculateSalary(Functionary functionary){
-        if (functionary.getSalary() > 3000){
-            return functionary.getSalary() * 0.8;
+        if (functionary.getRole().equals(Role.DEVELOPER)){
+            if (functionary.getSalary() > 3000){
+                return functionary.getSalary() * 0.8;
+            }
+            return functionary.getSalary() * 0.9;
+        } else if (functionary.getRole().equals(Role.DBA)
+                    || functionary.getRole().equals(Role.TESTER)) {
+            if (functionary.getSalary() < 2500){
+                return functionary.getSalary() * 0.85;
+            }
+            return functionary.getSalary() * 0.75;
         }
-        return functionary.getSalary() * 0.9;
+
+        throw new RuntimeException("Funcionário com cargo inválido");
     }
 }
--- src/test/java/service/SalaryCalculatorTest.java
+++ src/test/java/service/SalaryCalculatorTest.java
@@ -9,7 +9,7 @@ import static org.junit.Assert.*;
 public class SalaryCalculatorTest {
 
     @Test
-    public void ShouldCalculateSalaryForDevelopersWithSalaryBelowLimit(){
+    public void shouldCalculateSalaryForDevelopersWithSalaryBelowLimit(){
         SalaryCalculator calculator = new SalaryCalculator();
         Functionary developer = new Functionary("Marcos", 1500.00, Role.DEVELOPER);
 
@@ -19,7 +19,7 @@ public class SalaryCalculatorTest {
     }
 
     @Test
-    public void ShouldCalculateSalaryForDevelopersWithSalaryAboveLimit(){
+    public void shouldCalculateSalaryForDevelopersWithSalaryAboveLimit(){
         SalaryCalculator calculator = new SalaryCalculator();
         Functionary developer = new Functionary("Marcos", 4000.00, Role.DEVELOPER);
 
@@ -29,13 +29,43 @@ public class SalaryCalculatorTest {
     }
 
     @Test
-    public void ShouldCalculateSalaryForDBAWithSalaryBelowLimit(){
+    public void shouldCalculateSalaryForDBAWithSalaryBelowLimit(){
         SalaryCalculator calculator = new SalaryCalculator();
-        Functionary developer = new Functionary("Marcos", 500.00, Role.DBA);
+        Functionary developer = new Functionary("Marcos", 1500.00, Role.DBA);
 
         double salary = calculator.calculateSalary(developer);
 
-        assertEquals(500.00 * 0.85, salary, 0.00001);
+        assertEquals(1500.00 * 0.85, salary, 0.00001);
+    }
+
+    @Test
+    public void shouldCalculateSalaryForDBAWithSalaryAboveLimit(){
+        SalaryCalculator calculator = new SalaryCalculator();
+        Functionary developer = new Functionary("Marcos", 4500.00, Role.DBA);
+
+        double salary = calculator.calculateSalary(developer);
+
+        assertEquals(4500.00 * 0.75, salary, 0.00001);
+    }
+
+    @Test
+    public void shouldCalculateSalaryForTesterWithSalaryBelowLimit(){
+        SalaryCalculator calculator = new SalaryCalculator();
+        Functionary developer = new Functionary("Marcos", 1500.00, Role.TESTER);
+
+        double salary = calculator.calculateSalary(developer);
+
+        assertEquals(1500.00 * 0.85, salary, 0.00001);
+    }
+
+    @Test
+    public void shouldCalculateSalaryForTesterWithSalaryAboveLimit(){
+        SalaryCalculator calculator = new SalaryCalculator();
+        Functionary developer = new Functionary("Marcos", 4500.00, Role.TESTER);
+
+        double salary = calculator.calculateSalary(developer);
+
+        assertEquals(4500.00 * 0.75, salary, 0.00001);
     }
 
 }
